85605f777ea04d8a0f3db594c7935364fbe3dfae
Oleg Nizhnik <odomontois@gmail.com>
2020-12-24 11:27:01 +0300

    parallel_courses_ii.rs

--- src/leetcode/challenge/december/week4.rs
+++ src/leetcode/challenge/december/week4.rs
@@ -1,5 +1,6 @@
-use crate::leetcode::data::Tree;
+use crate::leetcode::data::{Tree, List};
 
+struct Solution();
 
 pub fn is_balanced(root: Tree) -> bool { balanced_depth(root).is_some() }
 
@@ -9,4 +10,44 @@ fn balanced_depth(root: Tree) -> Option<usize> {
     let l = balanced_depth(b.left.clone())?;
     let r = balanced_depth(b.right.clone())?;
     if l > r + 1 || r > l + 1 { None } else { Some(l.max(r) + 1) }
+}
+
+pub fn next_greater_element(n: i32) -> i32 {
+    let s = format!("{}", n);
+    let mut digs: Vec<_> = s.chars().rev().collect();
+    let mut prev = '0';
+    for (i, &c) in digs.iter().enumerate() {
+        if c >= prev { prev = c; } else {
+            let j = digs.iter().position(|&x| x > c).unwrap();
+            digs.swap(i, j);
+            // println!("{} {} {:?}", i, j, digs);
+            (&mut digs[0..i]).reverse();
+            return digs.into_iter().rev().collect::<String>().parse::<i32>().unwrap_or(-1);
+        }
+    }
+    -1
+}
+
+#[test]
+fn nge() {
+    println!("{}", next_greater_element(12));
+    println!("{}", next_greater_element(102));
+    println!("{}", next_greater_element(201));
+    println!("{}", next_greater_element(132000));
+    println!("{}", next_greater_element(320001));
+    println!("{}", next_greater_element(320010));
+    println!("{}", next_greater_element(320100));
+    println!("{}", next_greater_element(321000));
+}
+
+impl Solution {
+    pub fn swap_pairs(head: List) -> List {
+        let mut node = *head?;
+        let mut next = if let Some(n) = node.next { *n } else {
+            return Some(Box::new(node));
+        };
+        node.next = Self::swap_pairs(next.next);
+        next.next = Some(Box::new(node));
+        Some(Box::new(next))
+    }
 }
--- src/leetcode/hard/parallel_courses_ii.rs
+++ src/leetcode/hard/parallel_courses_ii.rs
@@ -2,37 +2,76 @@ use std::collections::HashMap;
 use crate::data::combinations::Combinations;
 
 pub fn min_number_of_semesters(n: i32, dependencies: Vec<Vec<i32>>, k: i32) -> i32 {
-    let mut courses = Courses::new(n as usize, dependencies.into_iter().map(|v| (v[0] as usize, v[1] as usize)));
-    courses.calc(k as usize) as i32
+    let deps = dependencies.into_iter().map(|v| (v[0] as usize, v[1] as usize));
+    let mut courses = Courses::new(n as usize, deps, k as usize);
+    courses.calc().unwrap_or(254) as i32
 }
 
-struct Courses { cache: HashMap<u16, u8>, req: Vec<usize>, seq: Vec<Vec<usize>>, ready: Vec<usize>, done: u16 }
+#[derive(Debug)]
+struct Courses { cache: HashMap<u16, Option<u8>>, req: Vec<usize>, seq: Vec<Vec<usize>>, ready: u16, done: u16, k: usize }
 
 impl Courses {
-    fn new(n: usize, deps: impl Iterator<Item=(usize, usize)>) -> Courses {
+    fn new(n: usize, deps: impl Iterator<Item=(usize, usize)>, k: usize) -> Courses {
         let mut seq = vec![vec![]; n];
         let mut req = vec![0; n];
-        let mut ready = vec![];
+        let mut ready = 0;
         for (r, s) in deps {
-            seq[r].push(s);
-            req[s] += 1;
+            seq[r - 1].push(s - 1);
+            req[s - 1] += 1;
         }
         for i in 0..n {
             if req[i] == 0 {
-                ready.push(i)
+                ready ^= 1 << i;
             }
         }
-        Courses { cache: HashMap::new(), done: 0, seq, req, ready }
+        Courses { cache: HashMap::new(), done: 0, seq, req, ready, k }
     }
-    fn calc(&mut self, k: usize) -> u8 {
-        if let Some(&res) = self.cache.get(&self.done) { return res; }
 
-        let take = k.min(self.ready.len());
-        for comb in Combinations::slice(&self.ready, take) {
+    fn ready_vec(&self) -> Vec<usize> {
+        (0..16).filter(|&i| (self.ready & (1 << i)) != 0).collect()
+    }
 
+    fn apply(&mut self, comb: &Vec<usize>, un: bool) {
+        // println!("{:?} {:?} {:b}", self, comb, self.ready);
+        let (add, check) = if un { (2, 1) } else { (0, 0) };
+        for &i in comb {
+            self.done ^= 1 << i;
+            self.ready ^= 1 << i;
+            for &j in &self.seq[i] {
+                self.req[j] += add;
+                self.req[j] -= 1;
+                if self.req[j] == check {
+                    self.ready ^= 1 << j;
+                }
+            }
         }
-
-
-        unimplemented!()
     }
+
+
+    fn calc(&mut self) -> Option<u8> {
+        if let Some(&res) = self.cache.get(&self.done) { return res; }
+        if self.done + 1 == 1 << self.req.len() { return Some(0); }
+        let ready_vec = self.ready_vec();
+        let take = self.k.min(ready_vec.len());
+        let res = Combinations::vec_copy(ready_vec, take).filter_map(|comb| {
+            self.apply(&comb, false);
+            let res = self.calc();
+            self.apply(&comb, true);
+            res.map(|x| x + 1)
+        }).min();
+        self.cache.insert(self.done, res);
+        res
+    }
+}
+
+#[test]
+fn test() {
+    fn check(deps: &[[i32; 2]], n: i32, k: i32, res: i32) {
+        let dependencies = deps.iter().map(|i| i.into_iter().cloned().collect()).collect();
+        assert_eq!(min_number_of_semesters(n, dependencies, k), res)
+    }
+
+    // check(&[[2, 1], [3, 1], [1, 4]], 4, 2, 3);
+    check(&[[2, 1], [3, 1], [4, 1], [1, 5]], 5, 2, 4);
+    // check(&[], 1, 2, 1);
 }
