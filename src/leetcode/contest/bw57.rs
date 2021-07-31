95290f6b3f9e7f4bf3ae4e495edac170525d9c61
Oleg Nizhnik <o.nizhnikov@tinkoff.ru>
2021-07-31 23:33:24 +0300

    ***

--- src/leetcode/contest/bw57.rs
+++ src/leetcode/contest/bw57.rs
@@ -26,13 +26,15 @@ pub fn smallest_chair(times: Vec<Vec<i32>>, target_friend: i32) -> i32 {
         } else if enter {
             place[i] = first;
             first += 1;
-            if let Some(end) = taken.remove(&(first + 1)) {
-                first = end
+            if let Some(end) = taken.remove(&first) {
+                first = end + 1
             }
         } else if place[i] < first {
-            taken.insert(place[i], first - 1);
+            if place[i] < first - 1 { 
+                taken.insert(place[i] + 1, first - 1);
+            }
             first = place[i];
-        } else if let Some((&start, &end)) = taken.range(..place[i]).next_back() {
+        } else if let Some((&start, &end)) = taken.range(..=place[i]).next_back() {
             taken.remove(&start);
             if start < place[i] {
                 taken.insert(start, place[i] - 1);
@@ -51,6 +53,66 @@ fn test_smallest_chair() {
     fn check(xs: &[[i32; 2]], tg: i32, exp: i32) {
         assert_eq!(exp, smallest_chair(xs.iter().map(|v| v.to_vec()).collect(), tg))
     }
+
+    check(
+        &[
+            [18, 19],
+            [10, 11],
+            [21, 22],
+            [5, 6],
+            [2, 3],
+            [6, 7],
+            [43, 44],
+            [48, 49],
+            [53, 54],
+            [12, 13],
+            [20, 21],
+            [34, 35],
+            [17, 18],
+            [1, 2],
+            [35, 36],
+            [16, 17],
+            [9, 10],
+            [14, 15],
+            [25, 26],
+            [37, 38],
+            [30, 31],
+            [50, 51],
+            [22, 23],
+            [3, 4],
+            [27, 28],
+            [29, 30],
+            [33, 34],
+            [39, 40],
+            [49, 50],
+            [15, 16],
+            [4, 5],
+            [46, 47],
+            [51, 52],
+            [32, 33],
+            [11, 12],
+            [28, 29],
+            [47, 48],
+            [36, 37],
+            [40, 41],
+            [42, 43],
+            [52, 53],
+            [41, 42],
+            [31, 32],
+            [23, 24],
+            [8, 9],
+            [19, 20],
+            [24, 25],
+            [26, 27],
+            [45, 46],
+            [44, 45],
+            [7, 8],
+            [13, 14],
+            [38, 39],
+        ],
+        8,
+        0,
+    );
 
     check(
         &[
