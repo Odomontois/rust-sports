5e535b8582772add5a8a119044b592fc38c4064a
Oleg Nizhnik <odomontois@gmail.com>
2021-08-06 11:50:13 +0300

    12213

--- src/leetcode/challenge/august/largest_island.rs
+++ src/leetcode/challenge/august/largest_island.rs
@@ -1,64 +1,65 @@
-use std::collections::{HashSet, VecDeque};
+// use std::collections::{HashSet, VecDeque};
+// extern crate data;
 
-use data::neighbors::Neigbors;
+// use data::neighbors::Neigbors;
 
-pub fn largest_island(mut grid: Vec<Vec<i32>>) -> i32 {
-    let neigh = Neigbors::matrix(&grid);
+// pub fn largest_island(mut grid: Vec<Vec<i32>>) -> i32 {
+//     let neigh = Neigbors::matrix(&grid);
 
-    let mut colour = 2;
-    let mut sizes = vec![];
-    for (i, j) in neigh.walk() {
-        if grid[i][j] != 1 {
-            continue;
-        }
-        grid[i][j] = colour;
-        let mut q: VecDeque<_> = vec![(i, j)].into_iter().collect();
-        let mut size = 1;
-        while let Some((x, y)) = q.pop_front() {
-            for (u, v) in neigh.neighbors(x, y) {
-                if grid[u][v] == 1 {
-                    grid[u][v] = colour;
-                    size += 1;
-                    q.push_back((u, v))
-                }
-            }
-        }
-        colour += 1;
-        sizes.push(size);
-    }
+//     let mut colour = 2;
+//     let mut sizes = vec![];
+//     for (i, j) in neigh.walk() {
+//         if grid[i][j] != 1 {
+//             continue;
+//         }
+//         grid[i][j] = colour;
+//         let mut q: VecDeque<_> = vec![(i, j)].into_iter().collect();
+//         let mut size = 1;
+//         while let Some((x, y)) = q.pop_front() {
+//             for (u, v) in neigh.neighbors(x, y) {
+//                 if grid[u][v] == 1 {
+//                     grid[u][v] = colour;
+//                     size += 1;
+//                     q.push_back((u, v))
+//                 }
+//             }
+//         }
+//         colour += 1;
+//         sizes.push(size);
+//     }
 
-    neigh
-        .walk()
-        .filter(|&(i, j)| grid[i][j] == 0)
-        .map(|(i, j)| {
-            neigh
-                .neighbors(i, j)
-                .map(|(x, y)| grid[x][y])
-                .collect::<HashSet<_>>()
-                .into_iter()
-                .map(|c| if c > 1 { sizes[c as usize - 2] } else { 0 })
-                .sum()
-        })
-        .max()
-        .unwrap_or((neigh.hsize * neigh.vsize - 1) as i32)
-        + 1
-}
+//     neigh
+//         .walk()
+//         .filter(|&(i, j)| grid[i][j] == 0)
+//         .map(|(i, j)| {
+//             neigh
+//                 .neighbors(i, j)
+//                 .map(|(x, y)| grid[x][y])
+//                 .collect::<HashSet<_>>()
+//                 .into_iter()
+//                 .map(|c| if c > 1 { sizes[c as usize - 2] } else { 0 })
+//                 .sum()
+//         })
+//         .max()
+//         .unwrap_or((neigh.hsize * neigh.vsize - 1) as i32)
+//         + 1
+// }
 
-fn check<const N: usize>(exp: i32, grid: &[[i32; N]]) {
-    assert_eq!(exp, largest_island(grid.iter().map(|v| v.to_vec()).collect()))
-}
+// fn check<const N: usize>(exp: i32, grid: &[[i32; N]]) {
+//     assert_eq!(exp, largest_island(grid.iter().map(|v| v.to_vec()).collect()))
+// }
 
-#[test]
-fn test1() {
-    check(3, &[[1, 0], [0, 1]])
-}
+// #[test]
+// fn test1() {
+//     check(3, &[[1, 0], [0, 1]])
+// }
 
-#[test]
-fn test2() {
-    check(4, &[[1, 1], [1, 0]])
-}
+// #[test]
+// fn test2() {
+//     check(4, &[[1, 1], [1, 0]])
+// }
 
-#[test]
-fn test3() {
-    check(4, &[[1, 1], [1, 1]])
-}
+// #[test]
+// fn test3() {
+//     check(4, &[[1, 1], [1, 1]])
+// }
--- src/leetcode/contest/bw57/describe_the_painting.rs
+++ src/leetcode/contest/bw57/describe_the_painting.rs
@@ -1,4 +1,75 @@
-#[allow(unused)]
+use std::collections::HashMap;
+
 pub fn split_painting(segments: Vec<Vec<i32>>) -> Vec<Vec<i64>> {
-    todo!()
+    let mut evs = HashMap::new();
+    for v in segments {
+        *evs.entry(v[0] as i64).or_insert(0) += v[2] as i64;
+        *evs.entry(v[1] as i64).or_insert(0) -= v[2] as i64;
+    }
+    let (mut cc, mut res) = (0, vec![]);
+    let mut evs: Vec<_> = evs.into_iter().collect();
+    evs.sort();
+    for v in evs.windows(2) {
+        cc += v[0].1;
+        if cc != 0 {
+            res.push(vec![v[0].0, v[1].0, cc]);
+        }
+    }
+    res
+}
+
+#[cfg(test)]
+fn check(ss: &[[i32; 3]], exp: &[[i64; 3]]) {
+    assert_eq!(
+        exp.iter().map(|v| v.to_vec()).collect::<Vec<_>>(),
+        split_painting(ss.iter().map(|v| v.to_vec()).collect())
+    )
+}
+
+#[test]
+fn test1() {
+    check(
+        &[[1, 4, 5], [1, 4, 7], [4, 7, 1], [4, 7, 11]],
+        &[[1, 4, 12], [4, 7, 12]],
+    );
+}
+
+#[test]
+fn test2() {
+    check(
+        &[[1, 7, 9], [6, 8, 15], [8, 10, 7]],
+        &[[1, 6, 9], [6, 7, 24], [7, 8, 15], [8, 10, 7]],
+    );
+}
+
+#[test]
+fn test3() {
+    check(&[[1, 4, 5], [4, 7, 7], [1, 7, 9]], &[[1, 4, 14], [4, 7, 16]])
+}
+
+#[test]
+fn test4() {
+    check(
+        &[
+            [4, 16, 12],
+            [9, 10, 15],
+            [18, 19, 13],
+            [3, 13, 20],
+            [12, 16, 3],
+            [2, 10, 10],
+            [3, 11, 4],
+            [13, 16, 6],
+        ],
+        &[
+            [2, 3, 10],
+            [3, 4, 34],
+            [4, 9, 46],
+            [9, 10, 61],
+            [10, 11, 36],
+            [11, 12, 32],
+            [12, 13, 35],
+            [13, 16, 21],
+            [18, 19, 13],
+        ],
+    );
 }
