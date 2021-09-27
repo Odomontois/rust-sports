dbba4cfc6f46480a0e311afc9f1ca9205615d86b
Oleg Nizhnik <odomontois@gmail.com>
2021-09-27 16:59:21 +0300

    ккк

--- src/leetcode/challenge/september/chessboard.rs
+++ src/leetcode/challenge/september/chessboard.rs
@@ -5,28 +5,28 @@ pub fn moves_to_chessboard(board: Vec<Vec<i32>>) -> i32 {
 fn solution(board: Vec<Vec<i32>>) -> Option<i32> {
     let n = board.len();
     let x = solve_line(n, |i, j| board[i][j])?;
-    let y = solve_line(n, |j, i| board[j][i])?;
+    let y = solve_line(n, |i, j| board[j][i])?;
     Some(x + y)
 }
 fn solve_line(n: usize, f: impl Fn(usize, usize) -> i32) -> Option<i32> {
-    let mut pattern = vec![true];
-    for i in 1..n {
+    let match_line = |i: usize| {
         if (0..n).all(|j| f(i, j) == f(0, j)) {
-            pattern.push(true);
+            Some(true)
         } else if (0..n).all(|j| f(i, j) == 1 - f(0, j)) {
-            pattern.push(false);
+            Some(false)
         } else {
-            return None;
+            None
         }
-    }
+    };
+    let pattern = (0..n).map(match_line).collect::<Option<Vec<_>>>()?;
     let s = pattern.iter().filter(|x| **x).count();
-    let u = (0..n).filter(|&j| j % 2 == 0 && !pattern[j]).count();
+    let u = (0..n).filter(|&j| j % 2 == 0 && pattern[j]).count();
     let res = if n % 2 == 0 && s * 2 == n {
-        u.min(n / 2 - u)
+        u.min(s - u)
     } else if n % 2 == 1 && s * 2 + 1 == n {
-        n / 2 - u
-    } else if n % 2 == 1 && s * 2 == n + 1 {
         u
+    } else if n % 2 == 1 && s * 2 == n + 1 {
+        s - u
     } else {
         return None;
     };
@@ -40,5 +40,3 @@ fn lol() {
         vec![Some(1), None, Some(2)].into_iter().collect::<Option<Vec<i32>>>()
     )
 }
-
-
