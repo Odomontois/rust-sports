fca733586358a9240dd82c6dfe7997e9828afa00
Oleg Nizhnik <odomontois@gmail.com>
2021-10-01 20:06:10 +0300

    ***

--- src/leetcode/challenge/september/longest_common_subseq.rs
+++ src/leetcode/challenge/september/longest_common_subseq.rs
@@ -3,14 +3,19 @@ pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
 }
 
 fn lcs<A: Eq>(a: &[A], b: &[A]) -> usize {
-    if a.len() > b.len() {
-        return lcs(b, a);
-    }
     (0..b.len()).fold(vec![0; a.len() + 1], |pl, j| {
-        let mut cl = vec![0; a.len() + 1];
-        for i in 0..a.len() {
-            cl[i + 1] = if a[i] == b[j] { pl[i] + 1 } else { cl[i].max(pl[i + 1]) };
-        }
-        cl
+        let f = |i, p: usize| if a[i] == b[j] { pl[i] + 1 } else { p.max(pl[i + 1]) };
+        scan(0..a.len(), 0, f).collect()
     })[a.len()]
+}
+
+fn scan<'a, A, B: Clone + 'a>(
+    it: impl Iterator<Item = A> + 'a,
+    b: B,
+    f: impl Fn(A, B) -> B + 'a,
+) -> impl Iterator<Item = B> + 'a {
+    it.scan(Some(b), move |b, a| {
+        *b = Some(f(a, b.take().unwrap()));
+        b.clone()
+    })
 }
