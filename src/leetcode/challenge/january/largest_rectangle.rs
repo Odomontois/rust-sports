b38f1b0f301b44b5e3800291965736a46295637e
Oleg Nizhnik <odomontois@gmail.com>
2022-01-29 10:46:05 +0300

    77777

---
+++ src/leetcode/challenge/january/largest_rectangle.rs
@@ -0,0 +1,19 @@
+pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
+    let left = dists_left(&heights);
+    let right = dists_left(heights.iter().rev());
+    let triples = left.into_iter().zip(right.into_iter().rev()).zip(heights);
+    triples.map(|((l, r), h)| h * (l + r - 1) as i32).max().unwrap_or(0)
+}
+
+fn dists_left<'a>(heights: impl IntoIterator<Item = &'a i32>) -> Vec<usize> {
+    let mut stack: Vec<(usize, i32)> = vec![];
+    let dist = |(i, &h): (usize, &i32)| {
+        while stack.last().filter(|&&(_, p)| p >= h).is_some() {
+            stack.pop();
+        }
+        let l = stack.last().map(|&(j, _)| i - j).unwrap_or(i + 1);
+        stack.push((i, h));
+        l
+    };
+    heights.into_iter().enumerate().map(dist).collect()
+}
--- src/leetcode/challenge/january/mod.rs
+++ src/leetcode/challenge/january/mod.rs
@@ -3,3 +3,4 @@ mod max_good_people;
 mod detect_capital;
 mod elements_in_binary_tree;
 mod add_and_search;
+mod largest_rectangle;
